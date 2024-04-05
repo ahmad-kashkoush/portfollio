399f9de1f476d453c4faa18590aa44f4f8db62f2
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-04-05 11:40:05 +0200

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <>
+     <header>
+        <nav>
+            <a href="#">Kashkoush.dev</a>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about</a></li>
+                <li><a href="#">skills</a></li>
+                <li><a href="#">projects</a></li>
+                <li><a href="#">services</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </nav>
+     </header>
+    </>
+  );
+}
