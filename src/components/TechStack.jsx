2095d68ed3428851f68f35bbe0f1b129d2b23ae0
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-06-01 15:30:25 +0300

    feat(TechStack): make techstack responsive

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,34 +1,52 @@
 /* eslint-disable react/prop-types */
-import { useAnimateSides } from "../hooks/useAnimateNav";
 
+const techStackIcons = [
+  {
+    alt: "HTML",
+    img: "https://skillicons.dev/icons?i=html",
+  },
+  {
+    alt: "CSS",
+    img: "https://skillicons.dev/icons?i=css",
+  },
+  {
+    alt: "JS",
+    img: "https://skillicons.dev/icons?i=js",
+  },
+  {
+    alt: "TS",
+    img: "https://skillicons.dev/icons?i=ts",
+  },
+  {
+    alt: "React",
+    img: "https://skillicons.dev/icons?i=react",
+  },
+  {
+    alt: "Tailwind",
+    img: "https://skillicons.dev/icons?i=tailwind",
+  },
+  {
+    alt: "Bootstrap",
+    img: "https://skillicons.dev/icons?i=bootstrap",
+  },
+];
 export function TechStack({ visible = true }) {
   return (
     <div
       className={`tech-stack 
-    stack-left
-    ${visible? "stack-get-from-side" : ""}`}
+    left-translate
+    ${visible ? "get-from-side" : ""}`}
     >
       <span>Tech Stack</span>
       <span className="breaker">|</span>
       <ul>
-        <li>
-          <img src="https://skillicons.dev/icons?i=html,css" alt="skill-icon" />
-        </li>
-        <li>
-          <img src="https://skillicons.dev/icons?i=js,ts" alt="skill-icon" />
-        </li>
-        <li>
-          <img
-            src="https://skillicons.dev/icons?i=react,next"
-            alt="skill-icon"
-          />
-        </li>
-        <li>
-          <img
-            src="https://skillicons.dev/icons?i=tailwind,scss"
-            alt="skill-icon"
-          />
-        </li>
+        {techStackIcons.map((icon) => {
+          return (
+            <li key={icon}>
+              <img src={icon.img} alt={icon.alt} />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
