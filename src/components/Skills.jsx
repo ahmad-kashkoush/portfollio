0a5d9b516fb4c4ad5ad18c67507ec1fe97881bdc
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-09-22 14:27:13 +0300

    feat: add skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import ProgressBar from "@ramonak/react-progress-bar";
+import { skills } from "../data/skills";
 import { useAnimateSides } from "../hooks/useAnimateNav";
 
 function Skills() {
@@ -13,9 +14,58 @@ function Skills() {
         >
           Skills{" "}
         </h2>
+        <div className="row skills">
+          <div className="max-w-full">
+            {skills &&
+              skills.map((item, idx) => (
+                <SkillSection skillBlock={item} key={idx} />
+              ))}
+          </div>
+        </div>
       </div>
     </section>
   );
 }
 
+function SkillSection({ skillBlock }) {
+  console.log(skillBlock);
+  return (
+    <ul className="grid sm:grid-cols-1 md:grid-cols-2 ">
+      <h3 className="col-span-full uppercase text-2xl text-center">
+        {skillBlock.skillsDescription}
+      </h3>
+      {skillBlock.skillsSection &&
+        skillBlock.skillsSection.map((skill, idx) => (
+          <SkillItem skill={skill} key={idx} />
+        ))}
+    </ul>
+  );
+}
+function SkillItem({ skill }) {
+  return (
+    <div className="m-8">
+      <li className="m-2">
+        <span className="font-bold text-[1.7rem] tracking-wide ">
+          {skill.skillname[0]}
+        </span>
+        <span
+          style={{
+            letterSpacing: "1px",
+          }}
+        >
+          {skill.skillname.slice(1)}
+        </span>
+      </li>
+      <ProgressBar
+        completed={skill.rate}
+        maxCompleted={100}
+        customLabel={`${skill.rate} %`}
+        className="wrapper"
+        bgColor={"#147efb"}
+        animateOnRender={true}
+        initCompletedOnAnimation={0}
+      />
+    </div>
+  );
+}
 export default Skills;
---
+++ src/data/skills.js
@@ -0,0 +1,200 @@
+export const skills = [
+    {
+        skillsDescription: "PROGRAMMING",
+        skillsSection: [
+            {
+                skillname: "C",
+                rate: "100",
+            },
+            {
+                skillname: "C++",
+                rate: "100",
+            },
+            {
+                skillname: "Python",
+                rate: "80",
+            },
+            {
+                skillname: "GO",
+                rate: "75",
+            },
+            {
+                skillname: "JavaScript",
+                rate: "75",
+            },
+            {
+                skillname: "SQL",
+                rate: "75",
+            },
+            {
+                skillname: "HTML",
+                rate: "70",
+            },
+            {
+                skillname: "CSS",
+                rate: "70",
+            },
+            {
+                skillname: "Java",
+                rate: "50",
+            },
+        ]
+    },
+    {
+        skillsDescription: "TOOLS and Concepts",
+        skillsSection: [
+            {
+                skillname: "GitHub",
+                rate: "100",
+            },
+            {
+                skillname: "GitLab",
+                rate: "100",
+            },
+            {
+                skillname: "Google Cloud",
+                rate: "80",
+            },
+            {
+                skillname: "Problem Solving",
+                rate: "100",
+            },
+            {
+                skillname: "Competitive Programming",
+                rate: "100",
+            },
+            {
+                skillname: "Data Structures",
+                rate: "100",
+            },
+            {
+                skillname: "Algorithms",
+                rate: "100",
+            },
+            {
+                skillname: "Object Oriented Programming",
+                rate: "90",
+            },
+            {
+                skillname: "Design Patterns",
+                rate: "70",
+            },
+            {
+                skillname: "System Design",
+                rate: "80",
+            },
+            {
+                skillname: "Trello",
+                rate: "70"
+            },
+            {
+                skillname: "Swagger",
+                rate: "95"
+            },
+            {
+                skillname: "Figma",
+                rate: "80"
+            },
+            {
+                skillname: "Postman",
+                rate: "90"
+            },
+            {
+                skillname: "Docker",
+                rate: "70",
+            },
+            {
+                skillname: "Kubernetes",
+                rate: "60",
+            }
+        ]
+    },
+    {
+        skillsDescription: "Miscellaneous",
+        skillsSection: [
+            {
+                skillname: "Git",
+                rate: "100",
+            },
+            {
+                skillname: "Bash",
+                rate: "70",
+            },
+            {
+                skillname: "Shell",
+                rate: "70",
+            },
+            {
+                skillname: "LaTeX",
+                rate: "90",
+            },
+            {
+                skillname: "Markdown",
+                rate: "100",
+            },
+            {
+                skillname: "Microsoft Office",
+                rate: "100",
+            },
+            {
+                skillname: "Web Scraping",
+                rate: "90",
+            },
+            {
+                skillname: "Google Documents (Docs, Sheets, Slides, Forms, etc.)",
+                rate: "100",
+            },
+            {
+                skillname: "Google API",
+                rate: "90",
+            },
+            {
+                skillname: "Web Extensions",
+                rate: "50",
+            }
+        ]
+    },
+    {
+        skillsDescription: "FRAMEWORKS",
+        skillsSection: [
+            {
+                skillname: "Django",
+                rate: "75",
+            },
+            {
+                skillname: "React JS",
+                rate: "80",
+            },
+            {
+                skillname: "React Native",
+                rate: "80",
+            },
+            {
+                skillname: "FastAPI",
+                rate: "80",
+            },
+            {
+                skillname: "Bootstrap",
+                rate: "60",
+            },
+
+        ]
+    },
+    {
+        skillsDescription: "OPERATING SYSTEMS",
+        skillsSection: [
+            {
+                skillname: "Windows",
+                rate: "100",
+            },
+            {
+                skillname: "Linux",
+                rate: "100",
+            },
+            {
+                skillname: "Mac OS",
+                rate: "100",
+            }
+        ]
+    },
+]
